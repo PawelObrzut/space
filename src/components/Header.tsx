e8140bd189f91a9c757ab31b4cb072cc6dccf497
PawelObrzut <pawel.r.obrzut@gmail.com>
2025-12-10 13:43:02 +0100

    fixes unused imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import NavBar from './NavBar'
 import logo from '../assets/shared/logo.svg'
 
--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 type Props = {
   webpSrc: string,
   pngSrc: string,
